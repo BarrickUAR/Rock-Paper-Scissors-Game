fef72e0526197d3519c2d786eaeddf14158aa97d
Berk Uçar <135522301+BarrickUAR@users.noreply.github.com>
2024-07-09 15:13:35 +0300

    Rules Added

--- assets/js/RPS.js
+++ assets/js/RPS.js
@@ -52,3 +52,23 @@ function playAgain(e) {
 }
 playagain.addEventListener('click', playAgain);
 
+document.addEventListener("DOMContentLoaded", function() {
+  let modal = document.getElementById("rulesModal");
+  let btn = document.getElementById("openRules");
+  let span = document.getElementsByClassName("close")[0];
+
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+});
